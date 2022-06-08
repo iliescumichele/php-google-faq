158d5da8d81933b796dcd8da8dd5466a94d28163
iliescumichele <iliescumichele@gmail.com>
2022-06-08 23:39:46 +0200

    init project

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+	Riscrivere questa pagina del sito google
+	https://policies.google.com/faq.
+	Ci sono diverse domande con relative risposte.
+	Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+	Per la formattazione del testo della FAQ inserite il markup HTML direttamente nel “database”
+*/
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
