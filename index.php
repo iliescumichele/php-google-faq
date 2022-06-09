42b2671ebb9d0238613102e003f3171c38061f43
iliescumichele <iliescumichele@gmail.com>
2022-06-09 15:54:36 +0200

    Added some styling + start leyout header

--- index.php
+++ index.php
@@ -124,17 +124,33 @@ $faqs = [
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
+	<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.css' integrity='sha512-1hsteeq9xTM5CX6NsXiJu3Y/g+tj+IIwtZMtTisemEv3hx+S9ngaW4nryrNcPM4xGzINcKbwUJtojslX2KG+DQ==' crossorigin='anonymous'/>
+
 	<link rel="stylesheet" href="style.css">
 	<title>Document</title>
 </head>
 <body>
 	
+	<header>
+        <ul class="d-flex justify-content-between">
+            <li class="mx-4 pt-2 list-style-none">
+                <img src="./img/logo-extended.jpg" alt="">
+                <a class="py-4" id="title-next-to-logo" href="#">Privacy e termini</a>
+            </li>
+            <li class="mx-4 pt-2 no-style my-2">
+                <i class="fa-solid fa-table-cells px-3"></i>
+                <i class="fa-solid fa-circle"></i>
+            </li>
+        </ul>
+    </header>
 
 	<main>
-		<?php foreach($faqs as $faq) : ?>
-        	<h4><?php echo $faq['question'] ?></h4>
-        	<?php echo $faq['answer'] ?>
-        <?php endforeach;?>
+		<div class="container">
+			<?php foreach($faqs as $faq) : ?>
+				<h2><?php echo $faq['question'] ?></h2>
+				<?php echo $faq['answer'] ?>
+			<?php endforeach;?>
+		</div>
 	</main>
 
 
